0b8ef250e7f8d62630f246af4220f1dcd75f3e54
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-07-09 14:03:37 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Made By [Potterhub👿](https://t.me/potterhub)",
+                    text="SPONSORED BY [TG_Movies4u](https://t.me/TG_Movies4u)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Owner💻", url=f"https://t.me/Pankajpandiyar")
+                                InlineKeyboardButton("Owner💻", url=f"https://t.me/brucewayne0070")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe ❤️", url=f"https://youtube.com/@mgod4469")
+                                InlineKeyboardButton("Subscribe ❤️", url=f'https://t.me/TG_Movies4u")
                             ]
                         ]
                     ),
