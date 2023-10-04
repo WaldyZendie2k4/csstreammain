c1bc9386b5d99a74f8f2e376b15b1bd87aa78202
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-10-04 19:14:49 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,17 +9,17 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', "29802408"))
-    API_HASH = str(getenv('API_HASH', "a6f4235751213eb700d6ef19be1a4135"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "6045014003:AAEwc6JyKmzzfRnJcqkSeWdZUWp0knq7LGk"))
+    API_ID = int(getenv('API_ID', "23292000"))
+    API_HASH = str(getenv('API_HASH', "be15e99b531e98b996bd4518fffe162d"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "6691198147:AAFFZoZs6YbfH4Gqkwttnd5LGtn-l3Xbwjs"))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001955392494"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001951246507"))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "969099516").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1758343727").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = str(getenv('APP_NAME'))
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
@@ -37,6 +37,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "https://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ""))
+    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://AATHU:AAthu@cluster0.wkp49kj.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-10019512465079")).split())) 
